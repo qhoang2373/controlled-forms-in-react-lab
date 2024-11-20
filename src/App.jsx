59ed044b21490130b7a071339eec01b54d7c60f0
Quan <qhoang2373@gmail.com>
2024-11-19 20:53:42 -0800

    complete skeleton setup and add components file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 // src/App.jsx
+import './App.css';
+import Bookshelf from './Bookshelf.jsx';
 
-const App = () => {
-  return <h1>Hello world!</h1>;
-};
+
+
+// <---- Define Initial state ====> 
+
+const Bookshelf = () => {
+
+  const [books, setBooks] = useState([
+    { title: 'The Alchemist', author: 'Paulo Coelho' },
+    { title: 'Things Fall Apart', author: 'Chinua Achebe' },
+  ]);
+
+
 
 export default App;
-
