e516824ef7e42cfb5fa0e1234b0dad70747189e6
Quan <qhoang2373@gmail.com>
2024-11-19 22:04:11 -0800

    create title, Author Field, and button

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -10,17 +10,23 @@ const Bookshelf = () => {
 
 // <---- Create Event handlers ====> 
 
+    {/*handle Input Change function*/}
     const handleInputChange = (event) => { 
         setNewBook({...books, [event.target.name]: event.target.value})
-    
       }
     
-      
+{/*handle submit function*/}
       const handleSubmit = (event) => {
+        event.preventDefault()
+        setBooks({...books})
+        setNewBook({
+            title: "",
+            author: ""
+        })
 
+    }
 
-
-
+// <---- Form Creation ====> 
 
    // Bookshelf Component   
 return ()
@@ -28,9 +34,35 @@ return ()
 <div className="bookshelfDiv">
   <div className="formDiv">
     <h3>Add a Book</h3>
-    {/* Form will go here */}
-  </div>
-  <div className="bookCardsDiv">{/* Book cards will display here */}</div>
+    <form onSubmit={handleSubmit}>
+
+    <label htmlFor="title">Title: </label>
+    <input 
+    id="title" 
+    name="title" 
+    value={newBook.title} 
+    onChange={handleInputChange}
+    ></input>
+{/* TitleInput Field, inputs will allow users to enter the details for the book they want to add to the shelf.*/}
+    
+    <label htmlFor="author">Author: </label>
+    <input 
+     id="author"
+     name="author" 
+     value={newBook.author}
+     onChange={handleInput}
+     ></input>
+{/* Author Input Field, inputs will allow users to enter the details for the book they want to add to the shelf.*/}
+        
+<button type="submit">Add New Book</button>
+     </form>
+    </div>
+
+  <div className="bookCardsDiv">
+  {books.map((book, index) => (
+    
+    
+    </div>
 </div>
 </>
 
